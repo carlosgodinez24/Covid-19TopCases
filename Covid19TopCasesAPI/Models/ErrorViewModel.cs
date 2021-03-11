e4840649e658d212e6a65657f8ece0f4459cb91d
Carlos Godínez <cgodinez24001@gmail.com>
2021-03-11 03:13:49 -0600

    Initial commit

---
+++ Covid19TopCasesAPI/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Covid19TopCasesAPI.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Covid19TopCasesClassLibrary/ModelClass.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Covid19TopCasesClassLibrary
+{
+    #region Regions catalog
+    /// <summary>
+    /// List of regions
+    /// </summary>
+    public class RegionList
+    {
+        public List<Region> Data { get; set; }
+    }
+
+    /// <summary>
+    /// Region object
+    /// </summary>
+    public class Region
+    {
+        public string Iso { get; set; }
+        public string Name { get; set; }
+    }
+    #endregion
+}
