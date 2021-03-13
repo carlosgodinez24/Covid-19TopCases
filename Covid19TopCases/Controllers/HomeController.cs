a278232b49b7791cc298b5ce0d55b690a7f29bfc
Carlos Godínez <cgodinez24001@gmail.com>
2021-03-13 11:26:38 -0600

    Web MVC App: principal template done

--- Covid19TopCases/Controllers/HomeController.cs
+++ Covid19TopCases/Controllers/HomeController.cs
@@ -1,21 +1,23 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 using Covid19TopCases.Models;
+using Microsoft.Extensions.Configuration;
+using Covid19TopCasesClassLibrary;
+using System.Collections.Generic;
+using RestSharp;
+using System.Net;
+using Newtonsoft.Json;
 
 namespace Covid19TopCases.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly IConfiguration configuration;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(IConfiguration iConfig)
         {
-            _logger = logger;
+            configuration = iConfig;
         }
 
         public IActionResult Index()
@@ -32,6 +34,82 @@ namespace Covid19TopCases.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        /// <summary>
+        /// Service that provide the catalog of regions
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet(nameof(Regions))]
+        public ActionResult Regions()
+        {
+            try
+            {
+                //Get the parameters from appsettings.json
+                var regionsCatalogEndpoint = configuration.GetValue<string>("RESTfulAPISettings:RegionsCatalogEndpoint");
+                var xApiKey = configuration.GetValue<string>("RESTfulAPISettings:Headers:x-rapidapi-key");
+
+                //Parameters from appsettings.json are validated
+                if (string.IsNullOrEmpty(regionsCatalogEndpoint))
+                {
+                    throw new Exception("Regions catalog endpoint is not configured");
+                }
+                if (string.IsNullOrEmpty(xApiKey))
+                {
+                    throw new Exception("API key is not configured");
+                }
+
+                //RESTful API Regions Catalog Consumption and Deserialization
+                IRestResponse responseAPI = RESTfulApiResponse(regionsCatalogEndpoint, Method.GET, xApiKey);
+                var responseCatalog = JsonConvert.DeserializeObject<ServiceResponse>(responseAPI.Content);
+                var response = new ServiceResponse()
+                {
+                    Status = (int)responseAPI.StatusCode,
+                    Code = responseCatalog.Code,
+                    Message = responseCatalog.Message,
+                    ErrorMessage = responseCatalog.ErrorMessage,
+                    StackTrace = responseCatalog.StackTrace,
+                    TransactionDateTime = responseCatalog.TransactionDateTime,
+                    Data = JsonConvert.DeserializeObject<List<RegionObject>>(responseCatalog.Data.ToString())
+                };
+                return StatusCode((int)responseAPI.StatusCode, response);
+            }
+            catch (Exception ex)
+            {
+                var response = new ServiceResponse()
+                {
+                    Status = (int)HttpStatusCode.InternalServerError,
+                    Code = "ERROR",
+                    Message = "An internal error occurred while obtaining the regions catalog, please try again later.",
+                    ErrorMessage = "500 Internal Server Error | " + ex.Message,
+                    StackTrace = ex.StackTrace,
+                    TransactionDateTime = DateTime.Now,
+                    Data = new List<object>()
+                };
+                return StatusCode((int)HttpStatusCode.InternalServerError, response);
+            }
+        }
+
+        /// <summary>
+        /// Generic RestSharp function for consumption of RESTful API services
+        /// </summary>
+        /// <param name="endpoint">REST service endpoint</param>
+        /// <param name="method">HTTP Method</param>
+        /// <param name="xApiKey">RapidAPI key</param>
+        /// <param name="json">JSON request</param>
+        /// <returns></returns>
+        private static IRestResponse RESTfulApiResponse(string endpoint, Method method, string xApiKey, string json = null)
+        {
+            var client = new RestClient(endpoint);
+            var restRequest = new RestRequest(method);
+            restRequest.AddHeader("Accept-Encoding", "gzip, deflate");
+            restRequest.AddHeader("Content-Type", "application/json");
+            restRequest.AddHeader("x-rapidapi-key", xApiKey);
+            restRequest.AddParameter("undefined", json, ParameterType.RequestBody);
+            //Omission of SSL certificates for all requests made
+            //ServicePointManager.ServerCertificateValidationCallback += (sender, certificate, chain, sslPolicyErrors) => true;
+            IRestResponse restResponse = client.Execute(restRequest);
+            return restResponse;
         }
     }
 }
--- Covid19TopCases/wwwroot/js/site.js
+++ Covid19TopCases/wwwroot/js/site.js
@@ -1,4 +1,70 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿/*
+=========================================================
+* AJAX REQUEST API
+=========================================================
+*/
+function executeAjaxAPI(url, request, requestType, callback) {
+    $('#loader').fadeIn();
+    $.ajax({
+        cache: false,
+        url: url,
+        data: JSON.stringify(request),
+        dataType: 'json',
+        type: requestType,
+        contentType: 'application/json; charset=utf-8',
+        statusCode: {
+            200: function (data) {
+                console.info('[site.js] StatusCode Response => 200 OK');
+                callback(data);
+                //$('#loader').fadeOut();
+            },
+            401: function (objError) {
+                console.warn('[site.js] StatusCode Response => 401 Unauthorized');
+                console.log(objError);
+                $('#loader').fadeOut();
+            },
+            400: function (objError) {
+                console.warn('[site.js] StatusCode Response => 400 Bad Request');
+                console.log(objError);
+                $('#loader').fadeOut();
+            },
+            404: function (objError) {
+                console.warn('[site.js] StatusCode Response => 404 Not Found');
+                console.log(objError);
+                $('#loader').fadeOut();
+            },
+            500: function (objError) {
+                console.warn('[site.js] StatusCode Response => 500 Internal Server Error');
+                console.log(objError);
+                $('#loader').fadeOut();
+            }
+        }
+    });
+}
 
-// Write your JavaScript code.
+/*
+=========================================================
+* DOCUMENT - READY
+=========================================================
+*/
+$(document).ready(function () {
+    getRegionsCatalog(window.RegionsCatalogEndpoint);
+});
+
+/*
+=========================================================
+* REGIONS CATALOG
+=========================================================
+*/
+function getRegionsCatalog(url) {
+    executeAjaxAPI(url, null, 'GET', function (response) {
+        console.log('[site.js] Service response => ' + url);
+        console.log(response);
+        let dropdown = $('#cmbRegion');
+        $.each(response.data, function (key, entry) {
+            dropdown.append($('<option></option>').attr('value', entry.iso).text(entry.name));
+        });
+        $('#loader').fadeOut(); //Comment later
+    });
+}
+
